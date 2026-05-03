d78dc2125fdc98969c2c000277e5f02f45381fad
lucllb3045 <=>
2026-05-03 19:11:01 -0300

    Agrega comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,6 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
+//Hace cositas
 int multiplicar(int a, int b) {
     return a * b;
 }
